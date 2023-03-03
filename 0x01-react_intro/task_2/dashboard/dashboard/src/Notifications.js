7afb695c04e6b93aeeb3139839e8c64e3977d04f
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-03 10:15:07 +0100

    Update Notifications.js

--- 0x01-react_intro/task_2/dashboard/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/dashboard/src/Notifications.js
@@ -1,10 +1,46 @@
-imort React from 'react';
-import './Notifications.css'
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from './utils';
+import closeIcon from './close-icon.png';
 
-export default function Notifications () {
+function Notifications() {
   return (
     <div className="Notifications">
-    <p>Here is the list of notifications</p>
+      <button style={{
+        color: '#3a3a3a',
+        fontWeight: 'bold',
+        background: 'none',
+        border: 'none',
+        fontSize: '15px',
+        position: 'absolute',
+        right: '3px',
+        top: '3px',
+        cursor: 'pointer',
+        outline: 'none',
+      }}
+      aria-label="Close"
+      onClick={(e) => {
+        console.log('Close button has been clicked');
+      }}
+      >
+        <img src={closeIcon} alt="close icon" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">
+					New course available
+				</li>
+				<li data-priority="urgent">
+					New resume available
+				</li>
+				<li
+					data-priority="urgent"
+					dangerouslySetInnerHTML={{__html: getLatestNotification()}}
+				>
+				</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
+
+export default Notifications;
