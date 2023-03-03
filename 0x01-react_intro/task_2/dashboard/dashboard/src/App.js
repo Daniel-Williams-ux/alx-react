e5b87bae927788ef9c846f600640a982c5fc30ce
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-03 09:35:52 +0100

    Update App.js

--- 0x01-react_intro/task_2/dashboard/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/dashboard/src/App.js
@@ -1,22 +1,27 @@
-import './App.css'
-import holberton_logo from './holberton_logo.jpg'
+import './App.css';
+import logo from './holberton_logo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <header className='App-header'>
-        <img src={holberton_logo} />
+      <div className="App-header">
+        <img src={logo} alt="logo" />
         <h1>School dashboard</h1>
-      </header>
-
-      <div className='App-body'>
-        <p>Login to access the full dashboard</p>
       </div>
-
-      <footer className='App-footer'>
-        <p>Copyright 2020 - holberton School</p>
-      </footer>
-      
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
     </div>
-  )
+  );
 }
+
+export default App;
