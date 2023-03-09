9e649f1a8f0f9a5c138e1be4989b5e89f264c117
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-09 18:23:19 +0100

    Update App.js

--- 0x02-react_props/task_0/dashboard/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/dashboard/src/App/App.js
@@ -1,31 +1,16 @@
-import './App.css';
-import Header from '../Header/Header
-import Login from '../Login/Login
-import Notifications from '../Notifications/Notifications
-import Footer from '../Footer/Footer
-import logo from '../assets/holberton_logo.jpg';
-import { getFooterCopy, getFullYear } from '../utils/utils';
+import './App.css'
+import Header from './components/Header';
+import Notifications from './components/Notifications';
+import Login from './components/Login';
+import Footer from './components/Footer';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Password: </label>
-        <input type="password" id="password" name="password" />
-        <button>OK</button>
-      </div>
-      <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
-      </div>
+    <div>
+      <Notifications />
+      <Header />
+      <Login />
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
