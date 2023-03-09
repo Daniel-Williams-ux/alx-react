96f64c3c7cd83fb6521bb1ba0ddcbbab6ee1bec0
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-09 17:41:03 +0100

    Update App.js

--- 0x02-react_props/task_0/dashboard/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/dashboard/src/App/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import Header from '../Header/Header
+import Login from '../Login/Login
+import Notifications from '../Notifications/Notifications
+import Footer from '../Footer/Footer
 import logo from '../assets/holberton_logo.jpg';
 import { getFooterCopy, getFullYear } from '../utils/utils';
 
