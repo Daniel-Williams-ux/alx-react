749c8f8c436e5e855a30f0cdd43f246813662255
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-11 14:53:56 +0100

    Update Notifications.js

--- 0x02-react_props/task_1/dashboard/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_1/dashboard/dashboard/src/Notifications/Notifications.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Notifications.css';
+import NotificationItem from './NotificationItem';
 import { getLatestNotification } from '../utils/utils';
 import closeIcon from '../assets/close-icon.png';
 
@@ -27,17 +28,9 @@ function Notifications() {
       </button>
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority="default">
-					New course available
-				</li>
-				<li data-priority="urgent">
-					New resume available
-				</li>
-				<li
-					data-priority="urgent"
-					dangerouslySetInnerHTML={{__html: getLatestNotification()}}
-				>
-				</li>
+        <NotificationItem type="default" value="New course available" />
+        <NotificationItem type="urgent" value="New resume available" />
+        <NotificationItem type="urgent" html={getLatestNotification()} />
       </ul>
     </div>
   );
