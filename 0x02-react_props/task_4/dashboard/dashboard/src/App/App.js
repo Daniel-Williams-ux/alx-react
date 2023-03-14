2cb6265d4e18e0d8baf5930a6cf1100e486f2c4e
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-14 16:53:40 +0100

    Update App.js

--- 0x02-react_props/task_4/dashboard/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/dashboard/src/App/App.js
@@ -3,14 +3,25 @@ import Header from './components/Header';
 import Notifications from './components/Notifications';
 import Login from './components/Login';
 import Footer from './components/Footer';
+import PropTypes from 'prop-types';
+import CourseList from '.components/CourseList';
 
-export default function App() {
+export default function App(props) {
+  const { isLoggedIn } = props;
   return (
     <div>
       <Notifications />
       <Header />
-      <Login />
+     {isLoggedIn ? <CourseList /> : <Login />}
       <Footer />
     </div>
   )
 }
+
+App.propTypes = {
+isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+isLoggedIn: false,
+};
