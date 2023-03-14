a06d9f4746dddc98a8042927484f242ca9770c4b
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-14 17:07:15 +0100

    Update Notifications.js

--- 0x02-react_props/task_4/dashboard/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_4/dashboard/dashboard/src/Notifications/Notifications.js
@@ -1,46 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Notifications.css';
 import { getLatestNotification } from '../utils/utils';
 import closeIcon from '../assets/close-icon.png';
+import PropTypes from 'prop-types';
 
-function Notifications() {
+function Notifications({ displayDrawer }) {
+  const [showNotifications, setShowNotifications] = useState(displayDrawer);
+
   return (
     <div className="Notifications">
-      <button style={{
-        color: '#3a3a3a',
-        fontWeight: 'bold',
-        background: 'none',
-        border: 'none',
-        fontSize: '15px',
-        position: 'absolute',
-        right: '3px',
-        top: '3px',
-        cursor: 'pointer',
-        outline: 'none',
-      }}
-      aria-label="Close"
-      onClick={(e) => {
-        console.log('Close button has been clicked');
-      }}
+      <div className="menuItem">Your notifications</div>
+      <button
+        style={{
+          color: '#3a3a3a',
+          fontWeight: 'bold',
+          background: 'none',
+          border: 'none',
+          fontSize: '15px',
+          position: 'absolute',
+          right: '3px',
+          top: '3px',
+          cursor: 'pointer',
+          outline: 'none',
+        }}
+        aria-label="Close"
+        onClick={(e) => {
+          console.log('Close button has been clicked');
+          setShowNotifications(false);
+        }}
       >
         <img src={closeIcon} alt="close icon" />
       </button>
-      <p>Here is the list of notifications</p>
-      <ul>
-        <li data-priority="default">
-					New course available
-				</li>
-				<li data-priority="urgent">
-					New resume available
-				</li>
-				<li
-					data-priority="urgent"
-					dangerouslySetInnerHTML={{__html: getLatestNotification()}}
-				>
-				</li>
-      </ul>
+      {showNotifications && (
+        <>
+          <p>Here is the list of notifications</p>
+          <ul>
+            <NotificationItem type="default" value="New course available" />
+            <NotificationItem type="urgent" value="New resume available" />
+            <NotificationItem
+              type="urgent"
+              html={getLatestNotification()}
+            />
+          </ul>
+        </>
+      )}
     </div>
   );
+}
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
 };
 
 export default Notifications;
