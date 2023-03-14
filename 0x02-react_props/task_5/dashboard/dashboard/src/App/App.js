41edbce83015b2b2c0d9d14935fee94cebf9bdc7
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-14 17:38:13 +0100

    Update App.js

--- 0x02-react_props/task_5/dashboard/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/dashboard/src/App/App.js
@@ -8,11 +8,17 @@ import CourseList from '.components/CourseList';
 
 export default function App(props) {
   const { isLoggedIn } = props;
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ];
   return (
     <div>
       <Notifications />
       <Header />
      {isLoggedIn ? <CourseList /> : <Login />}
+     <CourseList listCourses={listCourses} />
       <Footer />
     </div>
   )
