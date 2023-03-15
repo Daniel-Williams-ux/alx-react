63078c0dd5022a7a5de36e63d99ae221fb65dfde
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-15 20:53:47 +0100

    Update Notifications.js

--- 0x03-React_component/task_2/dashboard/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_2/dashboard/dashboard/src/Notifications/Notifications.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import './Notifications.css';
-import PropTypes from 'prop-types';
+import * as Utils from '../utils';
+import NotificationItem from './NotificationItem';
 
-class NotificationItems extends React.Component {
-  render () {
-    if (this.props.html) {
-      return (
-        <li data-notification-type={this.props.type}
-          dangerouslySetInnerHTML={{ __html: this.props.html }}
-          onClick={this.props.markAsRead(this.props.id)}></li>
-      );
-    }
-    return (
-      <li data-notification-type={this.props.type}
-        onClick={this.props.markAsRead(this.props.id)}>{this.props.value}
-      </li>
-    );
-    }
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
   }
 
-NotificationItems.propTypes = {
-  type: PropTypes.string,
-  value: PropTypes.string,
-  markAsRead: PropTypes.func
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="menuItems">Your notifications</div>
+        {this.props.displayDrawer
+          ? (
+            <div className="Notifications">
+              <button style={{"ariaLabel": "Close"}} onClick={console.log('Close button has been clicked')}>x</button>
+              <p>Here is the list of notifications</p>
+              <ul>
+                <NotificationItem markAsRead={this.markAsRead} id='1' type="default" value="New course available" />
+                <NotificationItem markAsRead={this.markAsRead} id='2' type="urgent" value="New resume available" />
+                <NotificationItem markAsRead={this.markAsRead} id='3' type="urgent" value="" html={Utils.getLatestNotification()} />
+              </ul>
+            </div>
+          )
+        : ( <div></div> )
+        }
+      </React.Fragment>
+    );
+  }
 }
 
-NotificationItems.default = {
-  type: 'default',
-  markAsRead: 0
+Notifications.defaultProps = {
+  displayDrawer: false
 }
 
-export default NotificationItems;
+export default Notifications;
