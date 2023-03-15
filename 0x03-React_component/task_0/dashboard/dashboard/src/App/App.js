c4852bf0acb47f2994c644b78e754330da1bc09e
Daniel Williams <williamsoligodaniel@gmail.com>
2023-03-15 20:33:00 +0100

    Update App.js

--- 0x03-React_component/task_0/dashboard/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/dashboard/src/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Notifications from './Notifications/Notifications';
 import Header from './Header/Header';
 import Login from './Login/Login';
@@ -6,23 +7,34 @@ import Footer from './Footer/Footer';
 import CourseList from './CourseList/CourseList';
 import './App.css';
 
+
 class App extends React.Component {
-  render () {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
+  
+  static defaultProps = {
+    isLoggedIn: false,
+  };
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
+    ];
+
     return (
-      <React.Fragment>
+      <div>
         <Notifications />
-        <div className="App">
-          <Header />
-          { !this.props.isLoggedIn ? <Login /> : <CourseList /> }
-          <Footer />
-        </div>
-      </React.Fragment>
+        <Header />
+        {isLoggedIn ? <CourseList /> : <Login />}
+        <CourseList listCourses={listCourses} />
+        <Footer />
+      </div>
     );
   }
-}
-
-App.defaultProps = {
-  isLoggedIn: false
 }
 
 export default App;
